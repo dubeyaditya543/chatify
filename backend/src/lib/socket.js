bd13b35b78dc9a4e55eb5518635248801e3c9304
Aditya Dubey <fordevelopmentusecase@gmail.com>
2026-08-14 23:45:02 +0530

    feat: implement real-time messaging using Socket.io and add notification sounds

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -14,9 +14,14 @@ export const io = new Server(server, {
   }
 })
 
+const userSocketMap = {}
+
+export function getReceiverSocketId(userId){
+  return userSocketMap[userId]
+}
+
 io.use(socketAuthMiddleware)
 
-const userSocketMap = {}
 
 io.on("connection", (socket) => {
   console.log("A user connected: ", socket.user.fullName)
